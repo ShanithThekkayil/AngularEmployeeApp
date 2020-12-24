432fd808710583a6f6a410677fa619af82ae7795
ShanithThekkayil <shaanzsster@gmail.com>
2020-12-24 13:45:24 +0530

    New Components Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDelDepComponent } from './Department/show-del-dep/show-del-dep.component';
+import { AddEditDepComponent } from './Department/add-edit-dep/add-edit-dep.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowDelEmpComponent } from './Employee/show-del-emp/show-del-emp.component';
+import { AddEditEmpComponent } from './Employee/add-edit-emp/add-edit-emp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    ShowDelDepComponent,
+    AddEditDepComponent,
+    EmployeeComponent,
+    ShowDelEmpComponent,
+    AddEditEmpComponent
   ],
   imports: [
     BrowserModule,
